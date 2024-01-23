e495edee2a39b2589533e5402875705522650c8b
abyrwalk <157430566+abyrwalk@users.noreply.github.com>
2024-01-23 11:48:31 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,87 @@
+# atomic-ton-recover
+
+This is a simple program that helps you move your TonCoin from an Atomic Wallet to a new wallet you own, as long as you have the secret information (like a secret key or a mnemonic phrase) from the old wallet.
+
+## Context
+For several months now, Atomic Wallet users have been experiencing issues with transferring TON to external wallets, stating that "TON sending operations might not work correctly in Atomic if your address is inactive on blockchain. ... Our team is working on the issue." And "Block explorer is currently unavailable".
+
+Given that TonCoins are indeed stored on the blockchain and Atomic Wallet allows for the export of secret keys, it should be possible to import these keys into any compatible wallet. However, a challenge arises because not many wallets neither support the 12-word mnemonic phrases, nor key the secret key export format utilized by Atomic Wallet. To address this inconvenience, I have developed this handy tool.
+
+## What This Tool Can Do For You
+* It lets you transfer TonCoin to a new wallet if you have either the special phrase (mnemonic phrase) or a secret key.
+* Think of it as a very basic version of a TonCoin wallet that can manage any TonCoin address you have the right to access.
+* This tool was created using an example from the TonCoin community, found here: https://github.com/toncenter/tonweb/blob/master/src/contract/wallet/README.md
+
+
+## Disclaimer
+This script is made available "as is," with no guarantees or warranties of any kind, whether stated or implied. Originally crafted for personal use to achieve a particular goal, it has been successfully deployed only once. The author disclaims all liability for any complications or damages that may result from utilizing this script. Your decision to use this script signifies your understanding and acceptance of any risks that may be involved. The script is shared with the community in the hope that it may be useful to others facing similar challenges.
+
+Should you require assistance with the operation of this tool, seek out a knowledgeable and trusted individual proficient in reading and executing JavaScript code. This person should be able to ensure that the script functions correctly and provide you with the necessary support.
+
+## What You Need Before Starting
+
+* Your secret key or the mnemonic phrase from your Atomic Wallet, and the address of the wallet. You can get the secret key by using the 12-word phrase you have from Atomic Wallet.
+* NodeJS version 20.0 or newer. You can find how to install it here: https://nodejs.org/en/learn/getting-started/how-to-install-nodejs
+* Basic knowledge of how to use the command line on your computer.
+* Git, which is a tool for downloading and updating code (this is optional, see Manual Installation below if you don't want to use Git).
+
+## How to Set Up the Tool
+### Easy Way: Using Git
+
+Copy the code and set up the tool and its dependencies by following these steps in your command line:
+
+```
+git clone https://github.com/abyrwalk/atomic-ton-recover.git
+cd atomic-ton-recover
+npm ci
+```
+
+### Manual Way: Without Git
+
+1. Download the script from this link: https://raw.githubusercontent.com/abyrwalk/atomic-ton-recover/main/recover.js and save it to a new folder, like Downloads/atomic-recover.
+2. Open your command line and go to the folder with `cd Downloads/atomic-recover`.
+3. Install the necessary parts by typing `npm install tonweb tonweb-mnemonic yargs`.
+
+## How to Use the Tool
+### Find Your Secret Key from Atomic Wallet
+
+* Open the Atomic Wallet App.
+* Go to More -> Security -> Private Keys & Backup.
+* Enter your wallet's password.
+* Click on Show Private Keys.
+* Read the warning, agree to it, and then find "Toncoin".
+* Click SHOW PRIVATE KEY and copy the long string of characters. Send it to yourself but keep it secret! You will need it in the next steps.
+* Alternatively you can export your 12-word backup phrase and use it with the tool `--mnemonic` option instead of `--privatekey`
+
+**Important**: Never share your secret key or mnemonic phrase! If someone else gets it, they can take your coins.
+
+### Find Your Wallet Address
+
+You probably already know this from when you've sent TonCoin before. If not:
+
+* Open Wallet -> TON -> Receive.
+* Copy your TonCoin address and send it to yourself.
+
+### Use the Tool to Move Your TonCoin
+
+Open the folder where you saved the recover.js script in the command line.
+Type `node recover.js --help` to make sure the tool is working correctly. It should show you how to use it.
+
+If everything looks fine, you can start moving your TonCoin.
+
+To send TonCoin from your old Atomic Wallet to your new wallet, make sure you know how much you have and use that amount in the command below. Please subtract 0.01 TON from the target amount for transfer fees. Here is an example command to transfer the coins:
+
+```
+node recover.js  --sourceAddress "your_atomic_wallet_address" --destinationAddress "your_new_wallet_address" --amount "0.01" --privatekey "your_128_chars_long_private_key" --dry-run 1
+```
+
+The --dry-run 1 part means that it will simulate the transfer without actually sending the coins. Remove --dry-run 1 when you're ready to make the real transfer.
+
+If the transaction fails try to reduce the amount for 0.01 TON and try again.
+
+## Donations
+If you've found the tool beneficial and would like to show your appreciation, consider buying me a coffee. You can do so by sending TonCoins to my non-Atomic wallet address:
+`UQDjx-YFsL1KvbXQQsspn0T96Ac0Wj4__Oo9n4w4meHGyGvP`
+
+# License
+Copyright (c) 2022 Abyrwalk. Licensed under the Apache 2.0 License
---
+++ recover.js
@@ -0,0 +1,178 @@
+// script to recover funds from a disfunctional TON wallet client
+// based on an example from https://github.com/toncenter/tonweb/blob/master/src/contract/wallet/README.md
+
+const tonweb = require("tonweb") // npm install tonweb@0.0.60
+const tonMnemonic = require("tonweb-mnemonic")
+const yargs = require('yargs/yargs');
+const { hideBin } = require('yargs/helpers');
+
+const TON_API_DELAY = 1000;
+
+function sleep(time) {
+    return new Promise((resolve) => setTimeout(resolve, time));
+}
+
+function getArgs() {
+
+    // Use yargs to parse command-line arguments
+    const argv = yargs(hideBin(process.argv))
+        .env("TON")
+        .option('sourceAddress', {
+            alias: 'src',
+            describe: 'Your source TON wallet address',
+            type: 'string',
+            demandOption: true,
+        })
+        .option('privateKey', {
+            alias: 'privatekey',
+            describe: 'Your private key (128 hex characters). Can be provided with an environment variable TON_PRIVATEKEY instead',
+            type: 'string',
+            // demandOption: true,
+        })
+        .option('mnemonic', {
+            describe: 'Your recovery mnemonic phrase (normally 12 or 24 words). Can be provided with an environment variable TON_MNEMONIC instead',
+            type: 'string',
+            // demandOption: true,
+        })
+        .option('destinationAddress', {
+            alias: 'dst',
+            describe: 'Destination TON address to move funds to',
+            type: 'string',
+            demandOption: true,
+        })
+        .option('amount', {
+            alias: 'amt',
+            describe: 'TON amount to send (e.g., 0.01)',
+            type: 'string',
+            default: '0.01',
+        })
+        .option('dry-run', {
+            alias: 'dry',
+            describe: 'do not send a transaction',
+            type: 'bool',
+            default: false,
+        })
+        .help()
+        .alias('help', 'h')
+        .argv;
+    return argv
+}
+
+
+async function getValidSeqno(wallet, maxRetries = 10, interval = 300) {
+    let retries = 0;
+    while (retries < maxRetries) {
+        const seqno = await wallet.methods.seqno().call();
+        if (seqno !== null) {
+            return seqno; // Return the valid seqno
+        }
+        // Wait for the interval before retrying
+        await sleep(interval);
+        retries++;
+        console.log("retrying to get seqno with attempt", retries);
+    }
+    throw new Error('Failed to retrieve a valid seqno after maximum retries.');
+}
+
+
+function arrayToHexString(byteArray) {
+    return Array.from(byteArray, function (byte) {
+        return ('0' + (byte & 0xFF).toString(16)).slice(-2);
+    }).join('')
+}
+
+
+async function recoverTon() {
+    // settings
+    /*
+    const srcAddr = "" // your source TON wallet address
+    const PKSrc = "" // your private key (128 hex characters)
+    const dstAddr = "" // destination TON address to move funds to
+    const amountToSend = '0.01' // 0.01 TON
+    */
+    const argv = getArgs()
+    console.log("args:", argv)
+    const srcAddr = argv.sourceAddress; // Your source TON wallet address
+    const PKSrc = argv.privatekey; // Your private key (128 hex characters)
+    const mnemonic = argv.mnemonic;
+    const dstAddr = argv.destinationAddress; // Destination TON address to move funds to
+    const amountToSend = argv.amount; // Amount to send (e.g., '0.01' TON)
+    // end of settings
+
+    const TonWeb = new tonweb();
+
+    var pkBytes = null;
+
+    if (mnemonic !== null) {
+        mnemonicArr = mnemonic.split(" ")
+        console.log("Mnemonic is set. Generating a private key")
+        keyPair = await tonMnemonic.mnemonicToKeyPair(mnemonicArr);
+        console.log("keypair", keyPair);
+        pkBytes = keyPair.secretKey
+        console.log("got secret key from mnemonic:", arrayToHexString(pkBytes));
+    } else pkBytes = TonWeb.utils.hexToBytes(PKSrc)
+
+    if (pkBytes === null) throw Error("Unable to parse secret key from the provided key or mnemonic")
+
+    const wallet = TonWeb.wallet.create({ address: srcAddr }); // if your know only address at this moment
+
+
+    const address = await wallet.getAddress();
+    console.log("my address: " + address)
+
+    await sleep(TON_API_DELAY);
+
+
+    const balance = await TonWeb.getBalance(address);
+    console.log("current balance: " + TonWeb.utils.fromNano(balance))
+    await sleep(TON_API_DELAY);
+
+
+    // var seqno = await wallet.methods.seqno().call()
+    const seqno = await getValidSeqno(wallet, 10, 300)
+    console.log("seqno:", seqno)
+
+    await sleep(TON_API_DELAY);
+
+
+    console.log("#### Trying to deploy the wallet")
+    const deploy = wallet.deploy(pkBytes)
+    console.log("Deployed:", deploy)
+
+    await sleep(TON_API_DELAY); // to avoid public api ratelimits
+
+    const deployFee = await deploy.estimateFee()
+
+    console.log("deploy fee: ", deployFee)
+
+
+
+    const transfer = wallet.methods.transfer({
+        secretKey: pkBytes,
+        toAddress: dstAddr,
+        amount: TonWeb.utils.toNano(amountToSend),
+        seqno: seqno,
+        payload: '',
+        sendMode: 3,
+    });
+    console.log("transfer:", transfer)
+    await sleep(TON_API_DELAY);
+
+    const transferFee = await transfer.estimateFee();   // get estimate fee of transfer
+    console.log("transferFee:", transferFee)
+    await sleep(TON_API_DELAY);
+
+    console.log("##### starting transfer:")
+    if (argv.dry) {
+        console.log("dry run: do not sending transfer");
+        return 0;
+    }
+    const transferSent = await transfer.send();  // send transfer query to blockchain
+    console.log("transfer sent:", transferSent)
+    await sleep(TON_API_DELAY);
+
+    // const transferQuery = await transfer.getQuery(); // get transfer query Cell, not needed actually
+    // console.log("transferQuery:", transferQuery)
+}
+
+recoverTon();
